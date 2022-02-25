cbd6c8a57729ae0a193a7ea1a53a6057d7c0d641
benkomlodi <benedek.komlodi@gmail.com>
2022-02-25 13:31:07 +0100

    Add files via upload

--- hazi/src/hazi/Titkosit.java
+++ hazi/src/hazi/Titkosit.java
@@ -1,11 +1,14 @@
 package hazi;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Titkosit {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Map<Character, Character> dictionary = new HashMap<Character, Character>();
         dictionary.put('á', 'a');
@@ -18,21 +21,32 @@ public class Titkosit {
         dictionary.put('ü', 'u');
         dictionary.put('ű', 'u');
 
-        if (args.length == 0) {
-            System.out.println("Nem adtál meg titkosítani kívánt szót!");
-        } else {
-            for(int i = 0; i<args[0].length(); i++) {
-                if (args[0].charAt(i) == 'z') {
-                    System.out.print("a");
-                } else if (dictionary.containsKey(args[0].charAt(i))) {
-                    int b = dictionary.get(args[0].charAt(i))+1;
-                    System.out.print((char) b);
-                } else {
-                    int a = args[0].charAt(i) + 1;
-                    System.out.print((char) a);
-                }
+        String line = null;
 
+        BufferedReader inFile = new BufferedReader(new FileReader(args[0]));
+        try {
+            line = inFile.readLine();
+            if (line == null) {
+                System.out.println("Nem adtál meg titkosítani kívánt szót a szöveges állományban!");
+            } else {
+                for(int i = 0; i<line.length(); i++) {
+                    if (line.charAt(i) == 'z') {
+                        System.out.print("a");
+                    } else if (dictionary.containsKey(line.charAt(i))) {
+                        int b = dictionary.get(line.charAt(i))+1;
+                        System.out.print((char) b);
+                    } else if (line.charAt(i) == ' ') {
+                        System.out.print(' ');
+                    } else {
+                        int a = line.charAt(i) + 1;
+                        System.out.print((char) a);
+                    }
+
+                }
             }
+        } catch (IOException e) {
+            System.out.println("Hiba történt!");
         }
+        inFile.close();
     }
 }
